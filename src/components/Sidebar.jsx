9bfb405b7f53d67165e8981c64fe1bbe43dcfacb
front-ed <edwinschutjes@gmail.com>
2023-07-29 13:56:05 +0200

    style: sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,45 @@
+import { social, links } from "../data.jsx";
+import { useGlobalContext } from "../context";
+import { FaTimes } from "react-icons/fa";
+
 const Sidebar = () => {
-  return <p>Sidebar component</p>;
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
+
+  return (
+    <aside className="sidebar">
+      <div className="sidebar-header">
+        <h4>COMPANY</h4>
+        <button className="close-btn" onClick={closeSidebar} type="button">
+          <FaTimes />
+        </button>
+      </div>
+      {/* navigation links */}
+      <ul className="links">
+        {links.map((link) => {
+          const { id, url, text, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon}
+                {text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      {/* social links */}
+      <ul className="social-links">
+        {social.map((link) => {
+          const { id, url, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
+  );
 };
 
 export default Sidebar;
